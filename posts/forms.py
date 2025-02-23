8622ee3f174c4c7a25edd03c3000b3d60a901f20
Erkinbekov-Bilim <erkinbekov298@gmail.com>
2025-02-23 23:05:31 +0600

    homework six

--- posts/forms.py
+++ posts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from posts.models import Category, Tag
 
 
 class PostCreateForm(forms.Form):
@@ -21,3 +22,20 @@ class PostCreateForm(forms.Form):
         if title and title.lower() == "title":
             raise forms.ValidationError("'title' is not a valid title")
         return title
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(required=False ,max_length=100, widget=forms.TextInput(attrs={"placeholder": "Search"}))
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="All", required=False)
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
+
+    orderings = (
+        ("title", "По заголовку"), # ascending - по возрастанию
+        ("-title", "По заголовку в обратном порядке"), # descending - по убыванию
+        ("rate", "По рейтингу"),
+        ("-rate", "По рейтингу в обратном порядке"),
+        ("created_at", "По дате создания"),
+        ("-created_at", "По дате создания в обратном порядке"),
+    )
+
+    ordering = forms.ChoiceField(choices=orderings, widget=forms.Select(attrs={"class": "form-select"}))
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, HttpResponse, redirect
-from posts.forms import PostCreateForm
+from django.template.defaultfilters import title
+from unicodedata import category
+
+from posts.forms import PostCreateForm, SearchForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Post
 import random
+
+
+"""
+
+posts = [post1, post2, post3, post4, post5, post6, post7, post8, post9, post10]
+limit = 2
+page = 3
+
+start = (page - 1) * limit
+end = start + limit
+
+start = (3 - 1) * 2 = 4
+end = 4 + 2 = 6
+
+"""
+
 
 
 
@@ -21,11 +41,56 @@ def html_view(request):
 
 @login_required(login_url="/login/")
 def post_list_view(request):
+    form = SearchForm()
+    query_params = request.GET # - Здесь мы получаем ключ и значение, которые передали в url
+    limit = 3
+
     if request.method == "GET":
         posts = Post.objects.all()
+        search = query_params.get("search")
+        category_id = query_params.get("category")
+        tags = query_params.getlist("tags")
+        ordering = query_params.get("ordering")
+        page = int(query_params.get("page")) if query_params.get("page") else 1
+
+
+        if search:
+            posts = Post.objects.filter(Q(title__icontains=search) | Q(content__icontains=search))
+
+        if category_id:
+            posts = posts.filter(category_id=category_id)
+
+        if tags:
+            tags = [int(tag) for tag in tags]
+            posts = posts.filter(tags__id__in=tags)
+
+        if ordering:
+            posts = posts.order_by(ordering) # order_by - сортировка по полю
+
+        max_pages = posts.count() / limit
+        # posts = 12 / 3 = 4.0
+
+        if round(max_pages) < max_pages:
+            max_pages = round(max_pages + 1)
+
+        else:
+            max_pages = round(max_pages)
+
+        start = (page - 1) * limit
+        end = start + limit
+
+        posts = posts[start:end] # slice - start, end, step
+
+        print(posts)
+
+        print(max_pages)
+
         context = {
-            "posts": posts
+            "posts": posts,
+            "form": form,
+            "max_pages": range(1, max_pages + 1),
         }
+
         return render(request, "posts/post_list.html", context = context)
     else:
         return HttpResponse("Hello World")
